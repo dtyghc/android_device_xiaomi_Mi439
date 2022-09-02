d114ffb5d02601d05f9e8e4e11ab384213fe0203
dtyghc <100001963+dtyghc@users.noreply.github.com>
2022-09-02 20:45:39 +0530

    Mi439: Updated Fingerprint to raven and added new prop
    Signed-off-by: karan2354 <karann2354@users.noreply.github.com>
    Signed-off-by: Abhi <cocabhi8020@gmail.com>

--- evolution_Mi439.mk
+++ evolution_Mi439.mk
@@ -13,9 +13,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 $(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
 # Build Flags
+USE_PROTON := true
 TARGET_BOOT_ANIMATION_RES := 720
-TARGET_USES_MINI_GAPPS := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_USES_BLUR := true
+TARGET_INCLUDE_PIXEL_CHARGER := true
+IS_PHONE := true
+TARGET_USES_MINI_GAPPS := true
 
 # Inherit from Mi439 device
 $(call inherit-product, device/xiaomi/Mi439/device.mk)
@@ -35,9 +39,8 @@ PRODUCT_MANUFACTURER := Xiaomi
 TARGET_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="olive-user 10 QKQ1.191014.001 V12.5.1.0.QCNMIXM release-keys"
+    PRIVATE_BUILD_DESC="olive-user 13 TPP2.220218.010 V12.5.1.0.QCNMIXM release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "Xiaomi/olive/olive:10/QKQ1.191014.001/V12.5.1.0.QCNMIXM:user/release-keys"
+BUILD_FINGERPRINT := "google/raven/raven:Tiramisu/TPP2.220218.010/8278900:user/release-keys"
